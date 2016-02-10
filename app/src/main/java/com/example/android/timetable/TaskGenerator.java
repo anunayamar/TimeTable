d9f73ec282f796a16d51765d3d2ea15356f418c6
anunayamar <anunay.amar17@gmail.com>
2016-02-10 00:03:05 -0500

    Enhancements

--- app/src/main/java/com/example/android/timetable/TaskGenerator.java
+++ app/src/main/java/com/example/android/timetable/TaskGenerator.java
@@ -9,13 +9,23 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
+import com.example.android.db.Task;
+import com.example.android.db.TaskDataSource;
+
+import java.sql.SQLException;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TaskGenerator extends FragmentActivity {
+
+    private TaskDataSource dataSource = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +59,7 @@ public class TaskGenerator extends FragmentActivity {
     }
 
     private void updateTimerLabel(int hourOfDay, int minute, boolean is24Hour){
-        
+
         TextView textView=(TextView)findViewById(R.id.timerlabel);
         textView.setTextSize(45);
         if(hourOfDay<=12) {
@@ -62,7 +72,45 @@ public class TaskGenerator extends FragmentActivity {
 
     public void showTimePickerDialog(View v) {
         DialogFragment newFragment = new TimePickerFragment();
-        newFragment.show(getFragmentManager(),"timePicker");
+        newFragment.show(getFragmentManager(), "timePicker");
+
+    }
+
+    public void saveTask(View view){
+        EditText taskTitle = (EditText) findViewById(R.id.taskTitle);
+        EditText taskDescription = (EditText) findViewById(R.id.taskDescription);
+        TextView taskTimer=(TextView) findViewById(R.id.timerlabel);
+        int []dayId= {R.id.sundayToggle, R.id.mondayToggle, R.id.tuesdayToggle, R.id.wednesdayToggle, R.id.thursdayToggle, R.id.fridayToggle, R.id.saturdayToggle};
+
+        Map<String, String> dayMap = new HashMap<>();
+        dayMap.put("MON","Monday");
+        dayMap.put("TUE","Tuesday");
+        dayMap.put("WED","Wednesday");
+        dayMap.put("THURS","Thursday");
+        dayMap.put("FRI","Friday");
+        dayMap.put("SAT","Saturday");
+        dayMap.put("SUN","Sunday");
+
+        dataSource = new TaskDataSource(this);
+        try{
+            dataSource.open();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        for(int i=0;i<7;i++){
+            ToggleButton dayToggle = (ToggleButton)findViewById(dayId[i]);
+
+            System.out.println(taskTitle.getText()+" "+taskDescription.getText()+" "+taskTimer.getText()+" "+dayToggle.isChecked()+" "+dayToggle.getTextOn());
+
+            if(dayToggle.isChecked()){
+                Task task = dataSource.createTask(dayMap.get(dayToggle.getTextOn().toString()), taskTitle.getText().toString(),
+                        taskDescription.getText().toString(), taskTimer.getText().toString());
+
+                System.out.println(task);
+            }
+        }
+        dataSource.close();
 
     }
 }
