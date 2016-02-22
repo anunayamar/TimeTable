de88cc431ed560de60848f4090026b417b141978
anunayamar <anunay.amar17@gmail.com>
2016-02-21 22:27:20 -0500

    Enhancements

--- app/src/main/java/com/example/android/timetable/TaskGenerator.java
+++ app/src/main/java/com/example/android/timetable/TaskGenerator.java
@@ -12,10 +12,15 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.example.android.db.Task;
 import com.example.android.db.TaskDataSource;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import java.sql.SQLException;
 import java.util.Calendar;
@@ -26,6 +31,7 @@ public class TaskGenerator extends FragmentActivity {
 
     private TaskDataSource dataSource = null;
     private String dayName;
+    private  int PLACE_PICKER_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -124,6 +130,34 @@ public class TaskGenerator extends FragmentActivity {
 
         finish();
 
+    }
+
+    public void showMap(View view){
+
+
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
